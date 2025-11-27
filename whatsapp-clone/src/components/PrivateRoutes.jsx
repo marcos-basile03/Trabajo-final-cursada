50ce2649f6b44929ef4f9eefa06c7284e2144c79
marcos.dev <marcosbasile11@gmail.com>
2025-11-26 23:12:43 -0300

    adsfsd

--- whatsapp-clone/src/components/PrivateRoutes.jsx
+++ whatsapp-clone/src/components/PrivateRoutes.jsx
@@ -1,18 +1,15 @@
-// src/components/PrivateRoute.jsx
 
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
-    // Revisa si existe el token en el almacenamiento local
     const token = localStorage.getItem('token');
     
     if (!token) {
-        // Si no hay token, lo redirige al login
         return <Navigate to="/login" replace />;
     }
     
-    return children; // Si hay token, muestra el componente (ej: Chat)
+    return children; 
 };
 
 export default PrivateRoute;
--- whatsapp-clone/src/main.jsx
+++ whatsapp-clone/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { ChatProvider } from './context/ChatContext';
+import AppRouter from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChatProvider>
-      <App />
+      <AppRouter /> 
     </ChatProvider>
   </React.StrictMode>
 );
