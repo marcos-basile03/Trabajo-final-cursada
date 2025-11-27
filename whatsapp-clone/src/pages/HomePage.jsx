ad784f71fa854faa6523ab43c12aae4dacb6d250
marcos.dev <marcosbasile11@gmail.com>
2025-11-26 23:33:19 -0300

    lola

--- whatsapp-clone/src/pages/HomePage.jsx
+++ whatsapp-clone/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
-import ContactList from '../components/ContactList';
+import ChatList from '../components/ChatList';
+
 
 export default function HomePage() {
   return (
     <div className="home-page">
       <h2>Chats</h2>
-      <ContactList />
+      <ChatList />
     </div>
   );
 }
